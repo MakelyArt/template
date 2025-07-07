62e3c6cecf39a08b189a460c1381d759179251a4
MakelyArt <makelyart@gmail.com>
2025-07-07 07:38:16 +0400

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -3,3 +3,8 @@ import '../styles/globals.css';
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
+<style jsx global>{`
+  html {
+    scroll-behavior: smooth;
+  }
+`}</style>
